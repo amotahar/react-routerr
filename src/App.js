c04c9ce7c4abba42411ee54b46f8feb22075e160
atuhin <atuhinbd@gmail.com>
2023-01-14 21:30:28 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Home from './component/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+        <Home></Home>
+    </div>
+  );
+}
+
+export default App;
+
+
+{/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
---
+++ src/component/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div>
+            
+            <>This is ok</>   
+            <Link>Thi is ok</Link> 
+        </div>
+    );
+};
+
+export default Home;
