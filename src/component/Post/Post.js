ae534c436b7586444822c7ada442433e7c9a9e47
atuhin <atuhinbd@gmail.com>
2023-01-17 13:13:24 +0600

    amidsf

--- src/component/Post/Post.js
+++ src/component/Post/Post.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './Post.css';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Post = ({post}) => {
-    const {title, body} = post;
+    const {title, body, id} = post;
+    const navigate = useNavigate()
+    const handleNavigate = ()=>{
+        navigate(`/post/${id}`);
+    }
     return (
         <div className='post'>
             
               <p>{title}</p>
               <p><small>{body}</small></p>
-              <button>Show more details</button>
+              <Link to={`/post/:${id}`}>Visit Id</Link>
+              <Link to={`/post/:${id}`}>
+                <button>Show Details</button>
+                </Link>
+            <button onClick={handleNavigate}> Show Details 2</button>
            
             
         </div>
